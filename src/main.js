f54ef264abd5c3e55ba6a8365d60d4082c02624e
JiWenBo <jwb9325@163.com>
2024-07-10 15:23:40 +0800

    添加 Vuetify UI 组件库

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { vuetify, i18n } from '../theme/ui/vuetify'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
+app.use(vuetify)
 
 app.mount('#app')
---
+++ theme/ui/vuetify.js
@@ -0,0 +1,44 @@
+import { createApp } from 'vue'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, md } from 'vuetify/iconsets/md'
+import 'vuetify/styles'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@/assets/style/main.scss'
+
+import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n'
+import { createI18n, useI18n } from 'vue-i18n'
+import zh from "../locale/zh.js"
+import en from "../locale/en.js"
+
+const messages = {
+    en: en,
+    zh: zh
+}
+
+const i18n = createI18n({
+    legacy: false, // Vuetify does not support the legacy mode of vue-i18n
+    locale: 'zh',
+    fallbackLocale: 'en',
+    messages,
+})
+const vuetify = createVuetify({
+    components,
+    directives,
+    locale: {
+        adapter: createVueI18nAdapter({ i18n, useI18n }),
+    },
+    icons: {
+        defaultSet: 'md',
+        aliases,
+        sets: {
+            md,
+        },
+    },
+
+})
+
+export { vuetify, i18n }
